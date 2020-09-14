cbfd1117dbf34e2f2ec5f5eb97d51514c830d2a3
Henrique Bustillos <henrique.ssbl@hotmail.com>
2020-09-14 14:06:02 -0300

    Requisições GET e POST

--- api/server.js
+++ api/server.js
@@ -2,6 +2,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     mongodb = require('mongodb');
+    objectId = require('mongodb').ObjectId;
 
 // Instanciar o express
 var app = express();
@@ -14,4 +15,60 @@ app.use(bodyParser.json());
 var port = 8080;
 app.listen(port);
 
+// Conexão com o mongodb
+var db = mongodb.Db(
+    'instagram',
+    new mongodb.Server('localhost', 27017, {}), {}
+);
+
+
 console.log('Servidor HTTP está escutando na porta ' + port);
+
+app.get('/', function(req, res) {
+    res.send({ msg: 'Olá' });
+});
+
+// API RESTful
+
+// POST (create)
+app.post('/api', function(req, res) {
+
+    // Recebendo dados enviados via POST
+    var dados = req.body;
+
+    db.open(function(err, mongoclient) {
+        mongoclient.collection('postagens', function(err, collection) {
+            collection.insert(dados, function(err, records) {
+
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(records);
+                }
+                mongoclient.close();
+            });
+        })
+    });
+
+});
+
+// GET by ID (read)
+app.get('/api/:id', function(req, res) {
+
+    // Recebendo dados enviados via POST
+    var dados = req.body;
+
+    db.open(function(err, mongoclient) {
+        mongoclient.collection('postagens', function(err, collection) {
+            collection.find(objectId(req.params.id)).toArray(function(err, results) {
+                if (err) {
+                    res.json(err);
+                } else {
+                    res.json(results);
+                }
+                mongoclient.close();
+            });
+        });
+    });
+
+});
